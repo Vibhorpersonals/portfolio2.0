502c88cc12655114141c3ce1e4067907cb728310
Vibhorpersonals <vibhorsharma969@gmail.com>
2024-08-05 20:18:50 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/portfolio2.0/', // Replace 'your-repo-name' with the name of your GitHub repository
 });
